a99da1a3f503d31e322dc5d8834ebfb40ddc89b8
Nicolás Snider <nsnider@soulitsolutions.com>
2020-05-30 13:48:29 -0300

    art detalle

--- src/app/articulo-detalle/articulo-detalle.component.ts
+++ src/app/articulo-detalle/articulo-detalle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Articulo } from '../models/Articulo';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-articulo-detalle',
@@ -13,6 +14,7 @@ export class ArticuloDetalleComponent implements OnInit {
 
   ngOnInit(): void {
     this.articulo=JSON.parse(this.ruta.snapshot.params.articulo);
+    console.log(this.articulo);
   }
 
 }
